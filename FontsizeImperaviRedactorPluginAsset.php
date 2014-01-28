84e98ed816804b6ce3c8d9030472623e289cbd51
Sorokin Victor <st-agrail@yandex.ru>
2014-01-28 21:26:01 +0200

    Update FontsizeImperaviRedactorPluginAsset.php

--- FontsizeImperaviRedactorPluginAsset.php
+++ FontsizeImperaviRedactorPluginAsset.php
@@ -5,7 +5,7 @@
  * @license http://www.yiiframework.com/license/
  */
 
-namespace yii\imperavi;
+namespace mitalcoi\imperavi;
 use yii\web\AssetBundle;
 
 /**
@@ -14,7 +14,7 @@ use yii\web\AssetBundle;
  */
 class FontsizeImperaviRedactorPluginAsset extends AssetBundle
 {
-    public $sourcePath = '@yii/imperavi/assets/plugins/fontsize';
+    public $sourcePath = '@mitalcoi/imperavi/assets/plugins/fontsize';
     public $js = [
         'fontsize.js'
     ];
@@ -22,6 +22,6 @@ class FontsizeImperaviRedactorPluginAsset extends AssetBundle
 
     ];
     public $depends = [
-        'yii\imperavi\ImperaviRedactorAsset'
+        'mitalcoi\imperavi\ImperaviRedactorAsset'
     ];
 }
